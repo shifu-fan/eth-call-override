be030d30700e66b15e15fbf58e2074386c55159b
liangqin.fan <liangqin.fan@okg.com>
2023-08-25 10:33:03 +0800

    call revert function

--- contracts/BaseViewer.sol
+++ contracts/BaseViewer.sol
@@ -5,7 +5,15 @@ import "./base.sol";
 
 contract BaseViewer is BaseContract{
 
+    error SetTestNumber(uint256 newNum, uint256 oldNum);
+
     function getNumber() external view returns(uint256) {
         return number;
     }
+
+    function setTestNumber(uint256 n) external {
+        uint256 old = number;
+        number = n;
+        revert SetTestNumber(n, old);
+    } 
 }
--- scripts/override.ts
+++ scripts/override.ts
@@ -1,10 +1,59 @@
 import { ethers } from "hardhat";
 import { promises as fs } from "fs";
 
+// from here:  https://github.com/devanoneth/TokenChecks/blob/master/scripts/tokenOverride.ts
+
 interface TokenParams {
   address: string;
 }
 
+async function callViewMethod(target: string, code: any) {
+  const BaseViewer = await ethers.getContractFactory("BaseViewer");
+    const functionData = BaseViewer.interface.encodeFunctionData("getNumber", []);
+
+    const returnedData = await ethers.provider.send("eth_call", [
+      {
+        data: functionData,
+        to: target,
+      },
+      "latest",
+      {
+        // state override set, the famous 3rd param of `eth_call` https://twitter.com/libevm/status/1476791869585588224
+        // we set the bytecode to the deployed bytecode of our "tolerance check override" contract
+        [target]: {
+          code,
+          // balance: utils.hexStripZeros(utils.parseEther("1").toHexString()),
+        },
+      },
+    ]);
+
+    console.log(`return data: ${returnedData}`)
+}
+
+async function callRevertMethod(target: string, code: any) {
+  const BaseViewer = await ethers.getContractFactory("BaseViewer");
+    const functionData = BaseViewer.interface.encodeFunctionData("setTestNumber", [
+      ethers.parseUnits("1.005", 3)
+    ]);
+
+    const returnedData = await ethers.provider.send("eth_call", [
+      {
+        data: functionData,
+        to: target,
+      },
+      "latest",
+      {
+        // state override set, the famous 3rd param of `eth_call` https://twitter.com/libevm/status/1476791869585588224
+        // we set the bytecode to the deployed bytecode of our "tolerance check override" contract
+        [target]: {
+          code,
+          // balance: utils.hexStripZeros(utils.parseEther("1").toHexString()),
+        },
+      },
+    ]);
+
+    console.log(`revert data: ${returnedData}`)
+}
 // Here, we use geth's state override set in eth_call to really simulate the blockchain
 export default async function tokenOverride(params: TokenParams): Promise<void> {
 
@@ -12,27 +61,10 @@ export default async function tokenOverride(params: TokenParams): Promise<void> 
     const ToleranceCheckOverrideDeployedBytecode = JSON.parse(
       await fs.readFile("./artifacts/contracts/BaseViewer.sol/BaseViewer.json", "utf-8"),
     ).deployedBytecode;
-
-    const BaseViewer = await ethers.getContractFactory("BaseViewer");
-    const functionData = BaseViewer.interface.encodeFunctionData("getNumber", []);
-
-    const returnedData = await ethers.provider.send("eth_call", [
-      {
-        data: functionData,
-        to: params.address,
-      },
-      "latest",
-      {
-        // state override set, the famous 3rd param of `eth_call` https://twitter.com/libevm/status/1476791869585588224
-        // we set the bytecode to the deployed bytecode of our "tolerance check override" contract
-        [params.address]: {
-          code: ToleranceCheckOverrideDeployedBytecode,
-          // balance: utils.hexStripZeros(utils.parseEther("1").toHexString()),
-        },
-      },
-    ]);
-
-    console.log(`return data: ${returnedData}`)
+    
+    await callViewMethod(params.address, ToleranceCheckOverrideDeployedBytecode);
+    await callRevertMethod(params.address, ToleranceCheckOverrideDeployedBytecode);
+    
   } catch (e) {
     console.error("FAILED ToleranceCheck: ", JSON.stringify(e, null, 2));
   }
